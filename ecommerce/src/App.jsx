20a724ea88116289ebc508f346216b9237d95992
Rofogale <marcoaureliodgomes@gmail.com>
2023-11-04 15:54:52 -0300

    inicializando Header com navbar

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,21 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Route, Routes} from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login'/>
-      <Route path='/categorias'/>
-      <Route path='/contato'/>
-      <Route path='/carrinho'/>
-      <Route path='/:jogo'/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" />
+        <Route path="/categorias" />
+        <Route path="/contato" />
+        <Route path="/carrinho" />
+        <Route path="/:jogo" />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ecommerce/src/pages/Home/index.jsx
+++ ecommerce/src/pages/Home/index.jsx
@@ -6,17 +6,17 @@ export default function Home() {
       <div className="background">
         <div className="gradiente"></div>
       </div>
-        <section id="inicio">
+      <section id="inicio">
         <h1>Logo?</h1>
-        </section>
-        <section id="destaques">
-            <h2>Destaques</h2>
-          <div class="grid-container">
-            <div class="conteudo-maior">jogo1</div>
-            <div class="conteudo-menor">jogo2</div>
-            <div class="conteudo-menor">jogo3</div>
-          </div>
-        </section>
+      </section>
+      <section id="destaques">
+        <h2>Destaques</h2>
+        <div class="grid-container">
+          <div class="conteudo-maior">jogo1</div>
+          <div class="conteudo-menor">jogo2</div>
+          <div class="conteudo-menor">jogo3</div>
+        </div>
+      </section>
     </main>
   );
 }
