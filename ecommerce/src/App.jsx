ad519fd489dc32f0718ab8ee7942d3ad673674d5
lucascaiafa00 <lucascaiafa00@gmail.com>
2023-11-04 15:08:52 -0300

    home e cores

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/'/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/login'/>
       <Route path='/categorias'/>
       <Route path='/contato'/>
---
+++ ecommerce/src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import "./index.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="background">
+        <div className="gradiente"></div>
+      </div>
+        <section id="inicio">
+        <h1>Logo?</h1>
+        </section>
+        <section id="destaques">
+            <h2>Destaques</h2>
+          <div class="grid-container">
+            <div class="conteudo-maior">jogo1</div>
+            <div class="conteudo-menor">jogo2</div>
+            <div class="conteudo-menor">jogo3</div>
+          </div>
+        </section>
+    </main>
+  );
+}
