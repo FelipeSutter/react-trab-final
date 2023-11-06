249bb4b38e233862b33b59d9f42223670d5021b0
Felipe Sutter <lipeps61@gmail.com>
2023-11-06 17:46:09 -0300

    fix: correção do nome no sobre nós

--- ecommerce/src/components/ImgAbout/index.jsx
+++ ecommerce/src/components/ImgAbout/index.jsx
@@ -6,7 +6,7 @@ function ImgAbout({ item }) {
   return (
     <a href={linkGithub} target="_blank" className="img-about">
       <img src={item.avatar_url} alt={item.login} />
-      <h2>{item.login}</h2>
+      <h2>{item.name}</h2>
     </a>
   );
 }
